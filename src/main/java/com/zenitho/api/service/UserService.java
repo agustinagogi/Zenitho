ec8dd96759ce45b98c887cc641bfc3c38612a731
Agustina <agustinatgg@gmail.com>
2025-08-28 00:32:28 +0200

    edit: added a way to assign roles to a new user

--- src/main/java/com/zenitho/api/service/UserService.java
+++ src/main/java/com/zenitho/api/service/UserService.java
@@ -1,18 +1,27 @@
 package com.zenitho.api.service;
 
+import com.zenitho.api.entities.ERole;
+import com.zenitho.api.entities.Role;
 import com.zenitho.api.entities.User;
+import com.zenitho.api.repositories.RoleRepository;
 import com.zenitho.api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authorization.method.AuthorizeReturnObject;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -24,6 +33,13 @@ public class UserService {
         newUser.setEmail(email);
 
         newUser.setPassword(passwordEncoder.encode(password));
+
+        // Asignar el rol por defecto
+        Set<Role> roles = new HashSet<>();
+        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                .orElseThrow(() -> new RuntimeException("Error: Role not found."));
+        roles.add(userRole);
+        newUser.setRoles(roles);
 
         return userRepository.save(newUser);
 
