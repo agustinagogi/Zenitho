c271c8edd123fc90120858ae1c66f84fc4db0ff4
Agustina <agustinatgg@gmail.com>
2025-08-27 20:33:57 +0200

    feat: create CardService service with create method

---
+++ src/main/java/com/zenitho/api/service/CardService.java
@@ -0,0 +1,37 @@
+package com.zenitho.api.service;
+
+import com.zenitho.api.entities.BoardColumn;
+import com.zenitho.api.entities.Card;
+import com.zenitho.api.repositories.BoardColumnRepository;
+import com.zenitho.api.repositories.CardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class CardService {
+
+    @Autowired
+    private CardRepository cardRepository;
+
+    @Autowired
+    private BoardColumnRepository columnRepository;
+
+    public Card createCard (String title, Long columnId) {
+        // Buscamos la columna para asegurarnos de que existe
+        BoardColumn column = columnRepository.findById(columnId)
+                .orElseThrow(() -> new RuntimeException("Column not found with id: " + columnId));
+
+        // Creamos la tarjeta
+
+        Card newCard = new Card();
+        newCard.setTitle(title);
+        newCard.setColumn(column);
+
+        // Guardamos la tarjeta en la BBDD y la devolvemos
+
+        return cardRepository.save(newCard);
+    }
+}
