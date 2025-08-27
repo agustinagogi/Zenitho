177300e5b17f32ebc683534959d3caaffb34a60a
Agustina <agustinatgg@gmail.com>
2025-08-27 20:15:47 +0200

    feat: created User entity with its atributes

--- src/main/java/com/zenitho/api/entities/User.java
+++ src/main/java/com/zenitho/api/entities/User.java
@@ -1,4 +1,33 @@
 package com.zenitho.api.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @ManyToMany(mappedBy = "members")
+    private Set<Board> boards;
+
+    @ManyToMany(mappedBy = "assigness")
+    private Set<Card> assignedCards;
 }
