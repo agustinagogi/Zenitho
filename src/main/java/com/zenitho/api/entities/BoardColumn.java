01e626657e9f22bcb18f169d74fa7833750a2d7a
Agustina <agustinatgg@gmail.com>
2025-08-27 19:57:51 +0200

    feat: created BoardColumn entity with its atributes

--- src/main/java/com/zenitho/api/entities/BoardColumn.java
+++ src/main/java/com/zenitho/api/entities/BoardColumn.java
@@ -1,4 +1,31 @@
 package com.zenitho.api.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name ="board_columns")
+@Data
 public class BoardColumn {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private double position;
+
+    // Relaciones
+    @ManyToOne // Muchas columnas pueden pertenecer a un solo tablero
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
+    // Una columna puede tener muchas tarjetas
+    // CascadeType.ALL si borramos una columna, todas las tarjetas que tiene dentro se borrarán automáticamente
+    @OneToMany(mappedBy = "column", cascade = CascadeType.ALL)
+    private List<Card> cards;
 }
