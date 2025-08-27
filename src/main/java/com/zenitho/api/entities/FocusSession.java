949ac24a8de446ff7dfed478cf9608102da92bc1
Agustina <agustinatgg@gmail.com>
2025-08-27 20:24:01 +0200

    feat: created FocusSession entity with its atributes

--- src/main/java/com/zenitho/api/entities/FocusSession.java
+++ src/main/java/com/zenitho/api/entities/FocusSession.java
@@ -1,4 +1,31 @@
 package com.zenitho.api.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "focus_sessions")
+@Data
 public class FocusSession {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    // Relaciones
+
+    @ManyToOne // Muchas sesiones pueden pertenecer a un solo usuario
+    @JoinColumn (name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne // Muchas sesiones pertencen a una sola tarjeta
+    @JoinColumn(name = "card_ id", nullable = false)
+    private Card card;
+
 }
