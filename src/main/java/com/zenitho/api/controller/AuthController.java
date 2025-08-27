130374542fd3368611fdcbb3db33a2ccb2ef9159
Agustina <agustinatgg@gmail.com>
2025-08-28 00:09:01 +0200

    feat: created AuthController to handle lgoin requests

---
+++ src/main/java/com/zenitho/api/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.zenitho.api.controller;
+
+import com.zenitho.api.jwt.JwtUtils;
+import com.zenitho.api.payload.LoginRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(loginRequest.getUsername());
+
+        return ResponseEntity.ok(jwt);
+    }
+}
