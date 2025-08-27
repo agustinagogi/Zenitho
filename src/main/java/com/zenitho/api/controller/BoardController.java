f88c0d589454f145ada90f0aa276b96e76b5301b
Agustina <agustinatgg@gmail.com>
2025-08-27 23:07:36 +0200

    feat: create getAllBoards(), getBoardById() on BoardController from BoardService

--- src/main/java/com/zenitho/api/controller/BoardController.java
+++ src/main/java/com/zenitho/api/controller/BoardController.java
@@ -3,10 +3,9 @@ package com.zenitho.api.controller;
 import com.zenitho.api.entities.Board;
 import com.zenitho.api.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/boards")
@@ -18,5 +17,15 @@ public class BoardController {
     @PostMapping
     public Board createBoard (@RequestParam String title, @RequestParam Long userId) {
         return boardService.createBoard(title, userId);
+    }
+
+    @GetMapping
+    public List<Board> getAllBoards () {
+        return boardService.getAllBoards();
+    }
+
+    @GetMapping("/{id}")
+    public Board getBoard (@PathVariable Long id) {
+        return boardService.getBoardById(id);
     }
 }
