2821974b4449ef86cfc7c94c67d30dd065460004
Agustina <agustinatgg@gmail.com>
2025-08-27 20:06:27 +0200

    feat: created Board entity with its atributes

--- src/main/java/com/zenitho/api/entities/Board.java
+++ src/main/java/com/zenitho/api/entities/Board.java
@@ -1,4 +1,35 @@
 package com.zenitho.api.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "boards")
+@Data
 public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    // Relaciones
+
+    // Un tablero contiene una lista de columnas
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
+    private List<BoardColumn> columns;
+
+    @ManyToMany
+    @JoinTable(
+            name = "board_members",
+            joinColumns = @JoinColumn(name = "board_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+
+    private Set<User> members;
 }
