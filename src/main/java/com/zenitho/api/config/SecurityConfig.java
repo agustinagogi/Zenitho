acd7fe5de0ceee4cc6a462eff95507b829e8acdf
Agustina <agustinatgg@gmail.com>
2025-08-27 23:00:17 +0200

    feat: create PasswordEncoder method

--- src/main/java/com/zenitho/api/config/SecurityConfig.java
+++ src/main/java/com/zenitho/api/config/SecurityConfig.java
@@ -3,7 +3,10 @@ package com.zenitho.api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
 
 @Configuration
 public class SecurityConfig {
@@ -17,5 +20,10 @@ public class SecurityConfig {
                         .anyRequest().authenticated() // Requerir autenticación para cualquier otra cosa
                 );
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
