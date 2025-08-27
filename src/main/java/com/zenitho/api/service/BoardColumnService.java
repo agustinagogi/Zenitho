21caf293fb4815bbc3b3641a59c254ef7d690189
Agustina <agustinatgg@gmail.com>
2025-08-27 23:25:42 +0200

    feat: create updateColumnTitle(), deleteColumn() on BoardColumnService

--- src/main/java/com/zenitho/api/service/BoardColumnService.java
+++ src/main/java/com/zenitho/api/service/BoardColumnService.java
@@ -36,4 +36,19 @@ public class BoardColumnService {
     public BoardColumn getColumnById(Long columnId){
         return columnRepository.findById(columnId).orElseThrow(() -> new RuntimeException("Column not found with id: " + columnId));
     }
+
+    public BoardColumn updateColumnTitle(Long columnId, String newTitle) {
+        BoardColumn existingColumn = columnRepository.findById(columnId)
+                .orElseThrow(() -> new RuntimeException("Column not found with id: " + columnId));
+
+        existingColumn.setTitle(newTitle);
+        return columnRepository.save(existingColumn);
+    }
+
+    public void deleteColumn(Long columnId) {
+        if (!columnRepository.existsById(columnId)) {
+            throw new RuntimeException("Column not found with id: " + columnId);
+        }
+        columnRepository.deleteById(columnId);
+    }
 }
