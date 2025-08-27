5a88b5f3afb7f30a981e12ca5630282381ae790d
Agustina <agustinatgg@gmail.com>
2025-08-27 19:51:06 +0200

    feat: created Card entity with its atributes

--- src/main/java/com/zenitho/api/entities/Card.java
+++ src/main/java/com/zenitho/api/entities/Card.java
@@ -1,4 +1,39 @@
 package com.zenitho.api.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "cards")
+@Data
 public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private int position;
+
+    private boolean archived = false;
+
+    @ManyToOne
+    @JoinColumn(name = "column_id", nullable = false)
+    private BoardColumn boardColumn;
+
+    @ManyToMany
+    @JoinTable(
+            name = "card_assignees",
+            joinColumns = @JoinColumn(name = "card_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> assignees;
 }
