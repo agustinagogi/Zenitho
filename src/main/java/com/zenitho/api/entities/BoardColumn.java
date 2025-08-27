62817adb87b0cac7205319fc3a4b98cc5f7400c7
Agustina <agustinatgg@gmail.com>
2025-08-27 19:26:16 +0200

    Initial commit

---
+++ src/main/java/com/zenitho/api/entities/BoardColumn.java
@@ -0,0 +1,4 @@
+package com.zenitho.api.entities;
+
+public class BoardColumn {
+}
---
+++ src/test/java/com/zenitho/api/ZenithoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenitho.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenithoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
