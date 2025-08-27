62ad3217d988bc2e2f301d0a87dd38d85440a240
Agustina <agustinatgg@gmail.com>
2025-08-28 00:33:20 +0200

    edit: include roles in UserDetailsServiceImpl

--- src/main/java/com/zenitho/api/service/UserDetailsServiceImpl.java
+++ src/main/java/com/zenitho/api/service/UserDetailsServiceImpl.java
@@ -2,12 +2,16 @@ package com.zenitho.api.service;
 
 import com.zenitho.api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.security.core.userdetails.User;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -20,7 +24,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         com.zenitho.api.entities.User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
 
-        // Retornamos una instancia de UserDetails que Spring Security necesita
-        return new User(user.getUsername(), user.getPassword(), new ArrayList<>());
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toList());
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
     }
 }
