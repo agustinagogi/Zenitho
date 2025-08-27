b7d8c440dcbd48319e72764a590dfed9b19efd0f
Agustina <agustinatgg@gmail.com>
2025-08-27 20:36:47 +0200

    feat: create cardController with endpoints

---
+++ src/main/java/com/zenitho/api/controller/CardController.java
@@ -0,0 +1,19 @@
+package com.zenitho.api.controller;
+
+import com.zenitho.api.entities.Card;
+import com.zenitho.api.service.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/cards")
+public class CardController {
+
+    @Autowired
+    CardService cardService;
+
+    @PostMapping
+    public Card createCard(@RequestParam String title, @RequestParam Long columnId) {
+        return cardService.createCard(title, columnId);
+    }
+}
