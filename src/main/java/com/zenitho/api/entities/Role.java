6f693aa4a9ca21841fd4daf34794af93bc838720
Agustina <agustinatgg@gmail.com>
2025-08-28 00:27:57 +0200

    feat: create Role to have different roles

---
+++ src/main/java/com/zenitho/api/entities/Role.java
@@ -0,0 +1,23 @@
+package com.zenitho.api.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+}
