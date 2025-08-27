84046d5fbd38f3df5292a254039315b3b72a90e0
Agustina <agustinatgg@gmail.com>
2025-08-28 00:43:23 +0200

    edit

--- src/main/java/com/zenitho/api/ZenithoApplication.java
+++ src/main/java/com/zenitho/api/ZenithoApplication.java
@@ -1,8 +1,12 @@
 package com.zenitho.api;
 
+import com.zenitho.api.entities.ERole;
+import com.zenitho.api.entities.Role;
+import com.zenitho.api.repositories.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ZenithoApplication {
@@ -11,4 +15,17 @@ public class ZenithoApplication {
 		SpringApplication.run(ZenithoApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(RoleRepository roleRepository) {
+		return args -> {
+			// Crear roles si no existen
+			if (roleRepository.findByName(ERole.ROLE_USER).isEmpty()) {
+				roleRepository.save(new Role(ERole.ROLE_USER));
+			}
+			if (roleRepository.findByName(ERole.ROLE_ADMIN).isEmpty()) {
+				roleRepository.save(new Role(ERole.ROLE_ADMIN));
+			}
+			// Puedes añadir más roles si los necesitas
+		};
+	}
 }
