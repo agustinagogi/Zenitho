798a6a5a8c33344c92ff6c2370257577db0e22f7
Agustina <agustinatgg@gmail.com>
2025-08-27 22:53:52 +0200

    feat: create BoardColumnService with createColumn method

---
+++ src/main/java/com/zenitho/api/service/BoardColumnService.java
@@ -0,0 +1,29 @@
+package com.zenitho.api.service;
+
+import com.zenitho.api.entities.Board;
+import com.zenitho.api.entities.BoardColumn;
+import com.zenitho.api.repositories.BoardColumnRepository;
+import com.zenitho.api.repositories.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardColumnService {
+
+    @Autowired
+    private BoardColumnRepository columnRepository;
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    public BoardColumn createColumn(String title, Long boardId){
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> new RuntimeException("Board not found with id: " + boardId));
+
+        BoardColumn newColumn = new BoardColumn();
+        newColumn.setBoard(board);
+        newColumn.setTitle(title);
+
+        return columnRepository.save(newColumn);
+    }
+}
