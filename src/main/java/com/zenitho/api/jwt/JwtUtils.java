9a0ec8c329d15b696a8fdbf428341c9204c92f25
Agustina <agustinatgg@gmail.com>
2025-08-28 17:47:37 +0200

    feat: create method to get the user ID from token

--- src/main/java/com/zenitho/api/jwt/JwtUtils.java
+++ src/main/java/com/zenitho/api/jwt/JwtUtils.java
@@ -1,9 +1,12 @@
 package com.zenitho.api.jwt;
 
+import com.zenitho.api.entities.User;
+import com.zenitho.api.repositories.UserRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.io.Decoders;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +19,9 @@ public class JwtUtils {
     @Value("${jwt.secret}")
     private String jwtSecret;
 
+    @Autowired
+    private UserRepository userRepository;
+
     private Key getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
         return Keys.hmacShaKeyFor(keyBytes);
@@ -23,16 +29,16 @@ public class JwtUtils {
 
     private static final long JWT_EXPIRATION_MS = 604800000; // 7 días
 
-    public String generateJwtToken(String username){
+    public String generateJwtToken(String email){
         return Jwts.builder()
-                .setSubject(username)
+                .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date((new Date()).getTime() + JWT_EXPIRATION_MS))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS512)
                 .compact();
     }
 
-    public String getUsernameFromJwtToken(String token) {
+    public String getEmailFromJwtToken(String token) {
         return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody().getSubject();
     }
 
@@ -44,6 +50,14 @@ public class JwtUtils {
             // Aquí puedes añadir logs para ver por qué falló la validación (token expirado, firma inválida, etc.)
             return false;
         }
+    }
+
+    // Nuevo método para obtener el ID del usuario del token
+    public Long getUserIdFromToken(String token) {
+        String email = getEmailFromJwtToken(token);
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found with email from token: " + email));
+        return user.getId();
     }
 
 }
