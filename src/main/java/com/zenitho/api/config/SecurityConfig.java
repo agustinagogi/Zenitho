58e4c198daf7eda3427f6bbd415810da6ef771c7
Agustina <agustinatgg@gmail.com>
2025-08-28 17:50:30 +0200

    edit: add another localhost port to make tests with

--- src/main/java/com/zenitho/api/config/SecurityConfig.java
+++ src/main/java/com/zenitho/api/config/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "https://tu-dominio.com"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "https://tu-dominio.com"));
         config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization"));
