6e88ba03ca7fe8c46f74cd7234809aebdae8d305
Agustina <agustinatgg@gmail.com>
2025-08-28 20:53:41 +0200

    feat: add query to find boards by member

--- src/main/java/com/zenitho/api/repositories/BoardRepository.java
+++ src/main/java/com/zenitho/api/repositories/BoardRepository.java
@@ -4,6 +4,9 @@ import com.zenitho.api.entities.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findByMembersId(Long userId);
 }
