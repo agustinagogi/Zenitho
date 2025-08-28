f65688a5f79890cdd8986abc0d0b3c0aa426c965
Agustina <agustinatgg@gmail.com>
2025-08-28 02:00:57 +0200

    feat: create method to search ssessions byId

--- src/main/java/com/zenitho/api/repositories/FocusSessionRepository.java
+++ src/main/java/com/zenitho/api/repositories/FocusSessionRepository.java
@@ -4,6 +4,9 @@ import com.zenitho.api.entities.FocusSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FocusSessionRepository extends JpaRepository<FocusSession, Long> {
+    List<FocusSession> findByUserId(Long userId);
 }
